ea4669aa263111d685537ce327a5881f4240175a
feldmatias <feldmatias@gmail.com>
2020-01-31 20:21:35 -0300

    fixed bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import 'react-native-gesture-handler';
+import 'reflect-metadata';
 import React from 'react';
 import {createStackNavigator} from 'react-navigation-stack';
 import {createAppContainer} from 'react-navigation';
