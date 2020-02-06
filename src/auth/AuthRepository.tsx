a595386cc272a297b6f4272c28032ec858f0294d
feldmatias <feldmatias@gmail.com>
2020-02-06 19:12:32 -0300

    implemented auth storage

--- src/auth/AuthRepository.tsx
+++ src/auth/AuthRepository.tsx
@@ -4,20 +4,32 @@ import {Result} from '../utils/Result';
 import {AuthToken} from './AuthToken';
 import {LoginMutation} from './login/LoginMutation';
 import {SignUpMutation} from './signup/SignUpMutation';
+import {AuthStorage} from './AuthStorage';
+import {GraphQLMutation} from '../graphql/GraphQLMutation';
 
 @injectable()
 export class AuthRepository {
 
     @inject(GraphQLService) private graphql!: GraphQLService;
 
+    @inject(AuthStorage) private storage!: AuthStorage;
+
     public async login(username: string, password: string): Promise<Result<AuthToken>> {
         const mutation = new LoginMutation(username, password);
-        return await this.graphql.mutation(mutation, AuthToken);
+        return await this.authMutation(mutation);
     }
 
     public async signUp(username: string, password: string): Promise<Result<AuthToken>> {
         const mutation = new SignUpMutation(username, password);
-        return await this.graphql.mutation(mutation, AuthToken);
+        return await this.authMutation(mutation);
+    }
+
+    private async authMutation(mutation: GraphQLMutation): Promise<Result<AuthToken>> {
+        const result = await this.graphql.mutation(mutation, AuthToken);
+        if (result.isSuccessful()) {
+            await this.storage.saveToken(result.getData());
+        }
+        return result;
     }
 }
 
--- src/di/Container.tsx
+++ src/di/Container.tsx
@@ -3,11 +3,19 @@ import {Container} from 'inversify';
 import {GraphQLService} from '../graphql/GraphQLService';
 import {AuthRepository} from '../auth/AuthRepository';
 import {GRAPHQL_DI, GraphQLClient, GraphQLClientInstance} from '../graphql/GraphQLClient';
+import {STORAGE_DI, StorageClient, StorageInstance} from '../storage/StorageClient';
+import {Storage} from '../storage/Storage';
+import {AuthStorage} from '../auth/AuthStorage';
 
 const DiContainer = new Container();
 
+DiContainer.bind<StorageClient>(STORAGE_DI).toConstantValue(StorageInstance);
+DiContainer.bind<Storage>(Storage).to(Storage);
+
 DiContainer.bind<GraphQLClient>(GRAPHQL_DI).toConstantValue(GraphQLClientInstance);
 DiContainer.bind<GraphQLService>(GraphQLService).to(GraphQLService);
+
+DiContainer.bind<AuthStorage>(AuthStorage).to(AuthStorage);
 DiContainer.bind<AuthRepository>(AuthRepository).to(AuthRepository);
 
 export default DiContainer;
